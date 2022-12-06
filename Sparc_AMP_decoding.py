ab9bf5f72657ad1240e32242746313e84ba99942
saidinesh75 <saidinesh75@gmail.com>
2022-12-06 18:40:55 +0530

    commit on 6_dec_2022

--- Sparc_AMP_decoding.py
+++ Sparc_AMP_decoding.py
@@ -12,7 +12,7 @@ import numpy.matlib
 import tensorflow.compat.v1 as tf
 
 rng = np.random.RandomState(seed=None)
-import pickle
+import pickle 
 
 from eta import eta
 from power_dist import power_dist
@@ -23,11 +23,10 @@ from tau_calculate import tau_calculate
 
 system_params   = {'P': 10.0,   # Average codeword symbol power constraint
                  'R': 0.5,      # Rate
-                 'L': 32,      # Number of sections
+                 'L': 256,      # Number of sections
                  'M': 512,       # Columns per section
-                 #'awgn_var':1,  # variance of noise
                  'snr_rx': 31,  # SNR at the receiver  (not in dB)   (SNR of 15 gives a capacity of 2 bits/sec)
-                 'dist': 0      # 0 for flat and 1 for exponential
+                 'dist': 1      # 0 for flat and 1 for exponential
                 }
 
 
@@ -43,23 +42,23 @@ beta, beta2, beta_val, beta2_val, new_system_params, SNR_params = generate_messa
 new_system_params_list = ['n','R_actual','delim','P_vec']
 n, R, delim,P_vec = map(new_system_params.get, new_system_params_list)
 
-
 # Extracting the SNR params
 SNR_params_list = ['snr_rx','awgn_var','capacity']
-# SNR_params_list = ['snr_rx','capacity']
 snr_rx,awgn_var,capacity = map(SNR_params.get, SNR_params_list)
 sigma = np.sqrt(awgn_var)
 
 
 # Matrix generation
-A_unn = np.random.normal(size=(n, N), scale=1.0 / math.sqrt(n)).astype(np.float32)
-A = A_unn/np.linalg.norm(A_unn,axis=0) 
+A = np.random.normal(size=(n, N), scale=1.0 / math.sqrt(n)).astype(np.float32)
+#A = A_unn/np.linalg.norm(A_unn,axis=0) 
 y = np.matmul(A,beta2) + np.random.normal( size = (n,cols), scale = math.sqrt(awgn_var) )
 
 
 # AMP Decoding
 T_star = int(np.ceil((2*capacity)/np.log2(capacity/R)) + 1)
-tau_vec= tau_calculate(awgn_var, delim,P,P_vec,T_star,n)
+tau_vec1, x= tau_calculate(awgn_var, delim,P,P_vec,T_star,n,rng)
+tau_vec = np.concatenate((tau_vec1[0].reshape([1,1]),tau_vec1), axis=0)
+
 
 beta_T = np.zeros((N,cols))
 beta_hat = np.zeros((N,cols))
@@ -68,7 +67,7 @@ z = np.zeros((n,cols))
 for t in range(T_star):
     z = y - np.matmul(A, beta_hat) + (z/tau_vec[t])*(P - (np.linalg.norm(beta_hat)**2)/n )
     beta_hat = beta_hat + np.matmul(A.T,z)
-    beta_hat = eta(beta_hat,P_vec, tau_vec[t],delim)
+    beta_hat = eta(beta_hat,P_vec, tau_vec[t],delim,n)
     
 for j in range(L):
     loc = np.argmax( beta_hat[ int(delim[0,j]) : int(delim[1,j]) ], axis=0 )
@@ -77,6 +76,8 @@ for j in range(L):
 
 check = beta-beta_T
 check_sum = np.sum(np.absolute(check),axis=0)/2
-check_avg = int(np.average(check_sum))
+check_avg = np.average(check_sum)
+# print(check_avg)
 output_params = system_params.copy()
 output_params['check_avg']=check_avg
+print(output_params)
